e451893e0d00e77b1007d39a40b0de8451cd8243
Harsh-Gowrisetty <harshithgow425@gmail.com>
2023-12-19 01:58:06 -0500

    work on buttons and about me section

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function openLinkInNewPage() {
+  // Replace 'your_link_url' with the actual URL you want to open in a new page
+  var linkUrl = 'your_link_url';
+    
+  // Open the link in a new tab/window
+  window.open(linkUrl, '_blank');
+}
